aca30f665c03d68996ea4c2129fe72fab5ed4d4c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-04-28 12:31:27 -0700

    Added binding, comparisons support for SpatialPolygons.

--- R/bind.R
+++ R/bind.R
@@ -5,6 +5,8 @@
 #' but until then, here we are.
 #'
 #' @param x,... Multiple \linkS4class{SpatialPoints} objects of the same dimensionality.
+#'
+#' Alternatively, multiple \linkS4class{SpatialPolygons} objects.
 #' @param ignore.mcols,recursive Further arguments passed to \code{\link{c,Vector-method}}.
 #'
 #' @return An object of the same class as \code{x} containing coordinates from all objects in \code{...}.
@@ -12,6 +14,7 @@
 #' @author Aaron Lun
 #'
 #' @examples
+#' # SpatialPoints:
 #' coords <- matrix(runif(30), nrow=10)
 #' X <- sp::SpatialPoints(coords)
 #'
@@ -20,11 +23,34 @@
 #'
 #' c(X, Y, X, Y) 
 #'
-#' @name bind
+#' # SpatialPolygons:
+#' spm <- makeSpatialPolygons(
+#'     rbind(c(0,0), c(0.5, 0.2), c(0.2, 0.6), c(0.9, 0.8)),
+#'     rbind(c(1,1), c(0.3, 0.7), c(0.1, 0.2), c(0.8, 0.3))
+#' )
+#'
+#' spm2 <- makeSpatialPolygons(
+#'     matrix(runif(10), ncol=2),
+#'     matrix(rnorm(8), ncol=2)
+#' )
+#'
+#' c(spm, spm2)
+#'
+#' @name spatula-bind
+#'
 #' @export
 #' @importMethodsFrom S4Vectors c
 setMethod("c", "SpatialPoints", function(x, ..., ignore.mcols = FALSE, recursive = FALSE) {
     stuff <- lapply(list(...), .PointsVector)
     out <- do.call(c, c(list(.PointsVector(x)), stuff, list(ignore.mcols=ignore.mcols, recursive=recursive)))
     out@points
+})
+
+#' @export
+#' @rdname spatula-bind
+#' @importMethodsFrom S4Vectors c
+setMethod("c", "SpatialPolygons", function(x, ..., ignore.mcols = FALSE, recursive = FALSE) {
+    stuff <- lapply(list(...), .PolygonsVector)
+    out <- do.call(c, c(list(.PolygonsVector(x)), stuff, list(ignore.mcols=ignore.mcols, recursive=recursive)))
+    out@polygons
 })
--- R/comparisons.R
+++ R/comparisons.R
@@ -6,44 +6,54 @@
 #' \code{order(..., na.last=TRUE, decreasing=FALSE, method=c("auto", "shell", "radix"))} 
 #' returns an integer permutation vector that arranges the first argument of \code{...} in ascending or descending order,
 #' see \code{?"base::order"} for more details.
-#' \code{...} should contain \linkS4class{SpatialPoints} objects of the same dimensionality.
+#' \code{...} should contain one or more \linkS4class{SpatialPoints} objects of the same dimensionality;
+#' or one or more \linkS4class{SpatialPolygons} objects.
 #'
-#' \code{sort(x, decreasing=FALSE, ...)} returns the \linkS4class{SpatialPoints} object \code{x} in sorted order,
-#' see \code{?"base::sort"} for more details.
+#' \code{sort(x, decreasing=FALSE, ...)} returns \code{x} in sorted order, see \code{?"base::sort"} for more details.
+#' \code{x} may be a \linkS4class{SpatialPoints} or \linkS4class{SpatialPolygons} object.
 #'
-#' Ordering of points inside a \linkS4class{SpatialPoint} object is determined by the value of the first dimension;
+#' Ordering of points inside a \linkS4class{SpatialPoints} object is determined by the value of the first dimension;
 #' and then the second dimension, if the first is tied; and so on for all available dimensions.
+#' Ordering of polygons inside a \linkS4class{SpatialPolygons} object is determined by the coordinates of the polygon centroid,
+#' with comparisons between successive dimensions as described above.
 #'
 #' @section Duplicates:
-#' \code{duplicated(x, incomparables=FALSE, ...)} returns a logical vector indicating whether a 
-#' of a \linkS4class{SpatialPoints} object \code{x} is a duplicate of a previous element.
-#' See \code{?"base::duplicated"} for more details.
+#' \code{duplicated(x, incomparables=FALSE, ...)} returns a logical vector indicating whether each entry of \code{x}
+#' is a duplicate of a previous elemen, see \code{?"base::duplicated"} for more details.
+#' \code{x} may be a \linkS4class{SpatialPoints} or \linkS4class{SpatialPolygons} object.
 #'
-#' \code{unique(x, incomparables=FALSE, ...)} returns the \linkS4class{SpatialPoints} object \code{x}
+#' \code{unique(x, incomparables=FALSE, ...)} returns \code{x}
 #' after removing all of the duplicated points, see \code{?"base::unique"} for more details.
+#' \code{x} may be a \linkS4class{SpatialPoints} or \linkS4class{SpatialPolygons} object.
 #'
-#' A point is considered as duplicate of another point if they have exactly the same coordinates.
+#' In a \linkS4class{SpatialPoints} object, a point is considered as duplicate of another point if they have exactly the same coordinates.
 #' No consideration is given towards numerical imprecision.
+#' In a \linkS4class{SpatialPolygons} object, polygons are considered to be duplicated if they have the same coordinates
+#' and other attributes (e.g., ring direction, whether or not it is a hole).
 #'
 #' @section Comparisons:
-#' Boolean operations like \code{x == y} can be performed if both \code{x} and \code{y}
-#' are \linkS4class{SpatialPoints} objects of the same dimensionality.
+#' Boolean operations like \code{x == y} can be performed if both \code{x} and \code{y} are:
+#' \itemize{
+#' \item two \linkS4class{SpatialPoints} objects of the same dimensionality.
+#' \item two \linkS4class{SpatialPolygons} objects.
+#' }
 #' This returns a logical vector indicating whether each point in \code{x} is equal to, less than or greater than 
 #' (depending on the operator) the corresponding point in \code{y}.
 #' If \code{x} and \code{y} are not of the same length, the shorter object is recycled.
 #'
 #' \code{match(x, table, nomatch=NA_integer, incomparables=FALSE, ...)} 
-#' returns an integer vector of length equal to the number of points in the \linkS4class{SpatialPoints} object \code{x}.
-#' Each element of this vector speciffies a point in the \linkS4class{SpatialPoints} object \code{table} 
-#' that is equal to the corresponding point in \code{x}.
+#' returns an integer vector of length equal to that of \code{x}.
+#' Each element of this vector specifies an entry in \code{table} that is equal to the corresponding entry in \code{x}.
 #'
-#' Points are compared to each other by considering the vectors of their coordinates.
+#' In \linkS4class{SpatialPoints} objects, points are compared to each other by considering the vectors of their coordinates.
 #' If all coordinates are exactly equal, so are the points;
 #' the first unequal coordinate is used to determine which point is \dQuote{greater than} or \dQuote{less than} the other.
+#' In \linkS4class{SpatialPolygons} objects, polygons are compared to each other by considering the vectors of their centroid coordinates.
 #'
 #' @author Aaron Lun
 #'
 #' @examples
+#' # For SpatialPoints:
 #' coords <- matrix(runif(30), nrow=10)
 #' X <- sp::SpatialPoints(coords)
 #'
@@ -57,10 +67,29 @@
 #' Y <- sp::SpatialPoints(coords2)
 #' X < Y
 #' X > Y
+#'
+#' # For SpatialPolygons:
+#' spm <- makeSpatialPolygons(
+#'     rbind(c(0,0), c(0.5, 0.2), c(0.2, 0.6), c(0.9, 0.8)),
+#'     rbind(c(1,1), c(0.3, 0.7), c(0.1, 0.2), c(0.8, 0.3))
+#' )
+#'
+#' order(spm)
+#' sort(spm)
+#' match(spm, spm[2:1])
+#' unique(c(spm, spm))
+#'
+#' spm==spm
+#' spm2 <- makeSpatialPolygons(
+#'     rbind(c(0,0), c(0.4, 0.2), c(0.2, 0.6), c(0.9, 0.8)),
+#'     rbind(c(1,1.1), c(0.3, 0.7), c(0.1, 0.2), c(0.8, 0.3))
+#' )
+#' spm < spm2
+#' spm > spm2
+#'
 #' @docType methods
-#' @name comparisons
+#' @name spatula-comparisons
 #' @aliases
-#' spatula-comparisons
 #' order,SpatialPoints-method
 #' sort,SpatialPoints-method
 #' unique,SpatialPoints-method
@@ -68,7 +97,19 @@
 #' Ops,SpatialPoints,SpatialPoints-method
 #' match,SpatialPoints,SpatialPoints-method
 #' %in%,SpatialPoints,SpatialPoints-method
+#'
+#' order,SpatialPolygons-method
+#' sort,SpatialPolygons-method
+#' unique,SpatialPolygons-method
+#' duplicated,SpatialPolygons-method
+#' Ops,SpatialPolygons,SpatialPolygons-method
+#' match,SpatialPolygons,SpatialPolygons-method
+#' %in%,SpatialPolygons,SpatialPolygons-method
 NULL
+
+#####################
+### SpatialPoints ###
+#####################
 
 #' @export
 setMethod("order", "SpatialPoints", function(..., na.last = TRUE, decreasing = FALSE, method = c("auto", "shell", "radix")) {
@@ -110,4 +151,50 @@ setMethod("match", c("SpatialPoints", "SpatialPoints"), function(x, table,
 #' @importFrom BiocGenerics %in%
 setMethod("%in%", c("SpatialPoints", "SpatialPoints"), function(x, table) {
     .PointsVector(x) %in% .PointsVector(table)
+})
+
+#######################
+### SpatialPolygons ###
+#######################
+
+#' @export
+setMethod("order", "SpatialPolygons", function(..., na.last = TRUE, decreasing = FALSE, method = c("auto", "shell", "radix")) {
+    vectorized <- lapply(list(...), .PolygonsVector)
+    do.call(order, c(vectorized, list(na.last=na.last, decreasing=decreasing, method=match.arg(method))))
+})
+
+#' @export
+setMethod("sort", "SpatialPolygons", function(x, decreasing = FALSE, ...) {
+    out <- sort(.PolygonsVector(x), decreasing=decreasing, ...)
+    out@polygons
+})
+
+#' @export
+setMethod("unique", "SpatialPolygons", function(x, incomparables=FALSE, ...) {
+    out <- unique(.PolygonsVector(x), incomparables=incomparables, ...)
+    out@polygons
+})
+
+#' @export
+setMethod("duplicated", "SpatialPolygons", function(x, incomparables=FALSE, ...) {
+    duplicated(.PolygonsVector(x), incomparables=incomparables, ...)
+})
+
+#' @export
+setMethod("Ops", c("SpatialPolygons", "SpatialPolygons"), function(e1, e2) {
+    callGeneric(.PolygonsVector(e1), .PolygonsVector(e2))
+})
+
+#' @export
+#' @importFrom BiocGenerics match
+setMethod("match", c("SpatialPolygons", "SpatialPolygons"), function(x, table, 
+    nomatch = NA_integer_, incomparables = NULL, ...)
+{
+    match(.PolygonsVector(x), .PolygonsVector(table), nomatch=nomatch, incomparables=incomparables, ...)
+})
+
+#' @export
+#' @importFrom BiocGenerics %in%
+setMethod("%in%", c("SpatialPolygons", "SpatialPolygons"), function(x, table) {
+    .PolygonsVector(x) %in% .PolygonsVector(table)
 })
