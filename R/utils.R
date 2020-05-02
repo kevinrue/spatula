92cddf52d42b9b6f73308c90c5416fd915a7a361
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-05-02 01:20:27 -0700

    Removed DF combining code.

--- R/utils.R
+++ R/utils.R
@@ -7,13 +7,3 @@
 .get_data <- function(sdf) sdf@data
 
 .get_proj4string <- function(s) s@proj4string
-
-.combine_df <- function(ref, others, N, ignore.mcols) {
-    if (ignore.mcols) {
-        data.frame(matrix(0L, N, 0L))
-    } else {
-        obj.df <- lapply(others, .get_data)
-        df <- c(list(.get_data(ref)), obj.df)
-        do.call(rbind, df)
-    }
-}
