ae113eda23574fd6cc1592170279d788783b6335
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-05-02 00:56:47 -0700

    Consolidated all S4Vectors bindings into a single file. Removed unnecessary user-level documentation.

--- R/bindings.R
+++ R/bindings.R
@@ -1,33 +1,44 @@
-#' ROWS methods for Spatial objects
+#' S4Vectors bindings for Spatial objects
 #'
-#' Implements methods for \code{\link{bindROWS}} and \code{\link{extractROWS}} 
-#' for \linkS4class{SpatialPoints} and \linkS4class{SpatialPolygons} objects,
-#' so that they are compatible with Bioconductor containers.
-#' See the \pkg{S4Vectors} package for more details.
+#' Implements various internal methods for \linkS4class{SpatialPoints} and \linkS4class{SpatialPolygons} objects,
+#' so that they are compatible with Bioconductor containers - see the \pkg{S4Vectors} package for more details.
 #'
 #' @author Aaron Lun
 #' @examples
 #' # Storing Spatial classes in containers:
 #' coords <- matrix(runif(30), nrow=10)
 #' X <- DataFrame(location=I(sp::SpatialPoints(coords)))
+#' X
 #' 
 #' coords2 <- matrix(runif(15), nrow=5)
 #' Y <- DataFrame(location=I(sp::SpatialPoints(coords2)))
+#' Y
 #'
 #' # Subsetting and such just works:
 #' rbind(X, Y)
 #' X[1:10,,drop=FALSE]
+#'
+#' # Works for spatial polygons as well, based on the polygon centroid.
+#' spm <- makeSpatialPolygons(matrix(runif(10), ncol=2), matrix(runif(10), ncol=2))
+#' pX <- DataFrame(poly=I(spm))
+#'
+#' spm2 <- makeSpatialPolygons(matrix(runif(10), ncol=2), matrix(runif(10), ncol=2))
+#' pY <- DataFrame(poly=I(spm2))
+#'
+#' rbind(pX, pY)
+#' pX[c(1,1,2,3,3),,drop=FALSE]
 #' 
 #' @docType methods
 #' @name spatula-bind
 #' @aliases
 #' bindROWS,SpatialPoints-method
 #' bindROWS,SpatialPolygons-method
-#' extractROWS,SaptialPolygons-method
+#' extractROWS,SpatialPolygons-method
+#' showAsCell,SpatialPoints-method
+#' showAsCell,SpatialPolygons-method
 NULL
 
 #' @export
-#' @rdname spatula-bind
 #' @importFrom S4Vectors bindROWS
 setMethod("bindROWS", "SpatialPoints", function(x, objects=list(), use.names=TRUE, ignore.mcols=FALSE, check=TRUE) {
     out <- bindROWS(.PointsVector(x), lapply(objects, .PointsVector),
@@ -36,7 +47,6 @@ setMethod("bindROWS", "SpatialPoints", function(x, objects=list(), use.names=TRU
 })
 
 #' @export
-#' @rdname spatula-bind
 #' @importFrom S4Vectors bindROWS
 setMethod("bindROWS", "SpatialPolygons", function(x, objects=list(), use.names=TRUE, ignore.mcols=FALSE, check=TRUE) {
     out <- bindROWS(.PointsVector(x), lapply(objects, .PointsVector),
@@ -45,7 +55,6 @@ setMethod("bindROWS", "SpatialPolygons", function(x, objects=list(), use.names=T
 })
 
 #' @export
-#' @rdname spatula-bind
 #' @importFrom S4Vectors extractROWS
 setMethod("extractROWS", "SpatialPolygons", function(x, i) {
     # WHAT A PAIN! Need to override sp's refusal to duplicate entries.
@@ -57,3 +66,19 @@ setMethod("extractROWS", "SpatialPolygons", function(x, i) {
 
     x[i]
 })
+
+#' @importFrom sp coordinates
+.coord_shower <- function(object) {
+    X <- coordinates(object)
+    collected <- lapply(seq_len(ncol(X)), function(i) showAsCell(X[,i,drop=FALSE]))
+    output <- do.call(paste, c(collected, list(sep=", ")))
+    sprintf("(%s)", output)
+}
+
+#' @export
+#' @importFrom sp SpatialPoints
+setMethod("showAsCell", "SpatialPoints", .coord_shower)
+
+#' @export
+#' @importFrom sp SpatialPolygons
+setMethod("showAsCell", "SpatialPolygons", .coord_shower) # uses the lab_pts, probably the average.
