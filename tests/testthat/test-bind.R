647b82b4963707b22f7673333fd5f2b302bab943
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-05-04 10:43:02 -0700

    Fixed bind tests to check for correct behavior inside DFrames.

--- tests/testthat/test-bind.R
+++ tests/testthat/test-bind.R
@@ -1,58 +1,65 @@
 # This tests the new binding functionality of Spatial classes.
 # library(testthat); library(spatula); source("setup.R"); source("test-bind.R")
 
-test_that("c() works for SpatialPoints", {
-    expect_identical(points1, c(points1))
+points1 <- DataFrame(loc=I(points1))
+points2 <- DataFrame(loc=I(points2))
+pointsdf1 <- DataFrame(loc=I(pointsdf1))
+pointsdf2 <- DataFrame(loc=I(pointsdf2))
+polys1 <- DataFrame(loc=I(polys1))
+polys2 <- DataFrame(loc=I(polys2))
+polysdf1 <- DataFrame(loc=I(polysdf1))
+polysdf2 <- DataFrame(loc=I(polysdf2))
 
-    out <- c(points1, points2)
-    expect_s4_class(out, "SpatialPoints")
-    expect_identical(coordinates(out), rbind(coordinates(points1), coordinates(points2)))
+test_that("rbind() works for SpatialPoints columns", {
+    expect_equivalent(points1, rbind(points1))
 
-    out2 <- c(out, points1)
-    expect_s4_class(out2, "SpatialPoints")
-    expect_identical(coordinates(out2), rbind(coordinates(out), coordinates(points1)))
+    out <- rbind(points1, points2)
+    expect_s4_class(out$loc, "SpatialPoints")
+    expect_identical(coordinates(out$loc), rbind(coordinates(points1$loc), coordinates(points2$loc)))
+
+    out2 <- rbind(out, points1)
+    expect_s4_class(out2$loc, "SpatialPoints")
+    expect_identical(coordinates(out2$loc), rbind(coordinates(out$loc), coordinates(points1$loc)))
 })
 
-test_that("c() works for SpatialPointsDataFrame", {
-    expect_identical(pointsdf1, c(pointsdf1))
+test_that("rbind() works for SpatialPointsDataFrame columns", {
+    expect_equivalent(pointsdf1, rbind(pointsdf1))
 
-    out <- c(pointsdf1, pointsdf2)
-    expect_s4_class(out, "SpatialPointsDataFrame")
-    expect_identical(coordinates(out), rbind(coordinates(pointsdf1), coordinates(pointsdf2)))
-    expect_identical(out$X, c(pointsdf1$X, pointsdf2$X))
+    out <- rbind(pointsdf1, pointsdf2)
+    expect_s4_class(out$loc, "SpatialPointsDataFrame")
+    expect_identical(coordinates(out$loc), rbind(coordinates(pointsdf1$loc), coordinates(pointsdf2$loc)))
+    expect_identical(out$loc$X, c(pointsdf1$loc$X, pointsdf2$loc$X))
 
-    out2 <- c(out, pointsdf1)
-    expect_s4_class(out2, "SpatialPointsDataFrame")
-    expect_identical(coordinates(out2), rbind(coordinates(out), coordinates(pointsdf1)))
-    expect_identical(out2$X, c(out$X, pointsdf1$X))
+    out2 <- rbind(out, pointsdf1)
+    expect_s4_class(out2$loc, "SpatialPointsDataFrame")
+    expect_identical(coordinates(out2$loc), rbind(coordinates(out$loc), coordinates(pointsdf1$loc)))
+    expect_identical(out2$loc$X, c(out$loc$X, pointsdf1$loc$X))
 })
 
-test_that("c() works for SpatialPolygons", {
-    expect_identical(polys1, c(polys1))
+test_that("rbind() works for SpatialPolygons columns", {
+    expect_equivalent(polys1, rbind(polys1))
 
-    out <- c(polys1, polys2)
-    expect_s4_class(out, "SpatialPolygons")
-    expect_equivalent(coordinates(out), rbind(coordinates(polys1), coordinates(polys2)))
-    expect_identical(0L, anyDuplicated(rownames(coordinates(out))))
+    out <- rbind(polys1, polys2)
+    expect_s4_class(out$loc, "SpatialPolygons")
+    expect_equivalent(coordinates(out$loc), rbind(coordinates(polys1$loc), coordinates(polys2$loc)))
+    expect_identical(0L, anyDuplicated(rownames(coordinates(out$loc))))
 
-    out2 <- c(out, polys1)
-    expect_s4_class(out2, "SpatialPolygons")
-    expect_equivalent(coordinates(out2), rbind(coordinates(out), coordinates(polys1)))
-    expect_identical(0L, anyDuplicated(rownames(coordinates(out2))))
+    out2 <- rbind(out, polys1)
+    expect_s4_class(out2$loc, "SpatialPolygons")
+    expect_equivalent(coordinates(out2$loc), rbind(coordinates(out$loc), coordinates(polys1$loc)))
+    expect_identical(0L, anyDuplicated(rownames(coordinates(out2$loc))))
 })
 
-test_that("c() works for SpatialPolygonsDataFrame", {
-    expect_equivalent(polysdf1, c(polysdf1))
+test_that("rbind() works for SpatialPolygonsDataFrame columns", {
+    expect_equivalent(polysdf1, rbind(polysdf1))
 
-    out <- c(polysdf1, polysdf2)
-    expect_s4_class(out, "SpatialPolygonsDataFrame")
-    expect_equivalent(coordinates(out), rbind(coordinates(polysdf1), coordinates(polysdf2)))
-    expect_identical(out$X, c(polysdf1$X, polysdf2$X))
+    out <- rbind(polysdf1, polysdf2)
+    expect_s4_class(out$loc, "SpatialPolygonsDataFrame")
+    expect_equivalent(coordinates(out$loc), rbind(coordinates(polysdf1$loc), coordinates(polysdf2$loc)))
+    expect_identical(out$loc$X, c(polysdf1$loc$X, polysdf2$loc$X))
 
-    out2 <- c(out, polysdf1)
-    expect_s4_class(out2, "SpatialPolygonsDataFrame")
-    expect_equivalent(coordinates(out2), rbind(coordinates(out), coordinates(polysdf1)))
-    expect_identical(out2$X, c(out$X, polysdf1$X))
+    out2 <- rbind(out, polysdf1)
+    expect_s4_class(out2$loc, "SpatialPolygonsDataFrame")
+    expect_equivalent(coordinates(out2$loc), rbind(coordinates(out$loc), coordinates(polysdf1$loc)))
+    expect_identical(out2$loc$X, c(out$loc$X, polysdf1$loc$X))
 })
-
-
